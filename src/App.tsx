cfaf36fef45132fbe0414ae55b9ab83cbac9abfe
GarrixA <aphrogarrix10@gmail.com>
2024-08-26 09:38:19 +0200

    dfsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0);
-  // const mar = 30;
-  // const text = "Let's go somewhere";
+  const [count, setCount] = useState(0);
+  const mar = 30;
+  const text = "Let's go somewhere";
 
   return (
     <>
